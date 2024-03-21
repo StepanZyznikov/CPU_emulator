f62b38c0cba19fa24263d1bee9de5006c7f27d85
StepanZyznikov <stepan.zyznikov@gmail.com>
2024-03-22 00:03:24 +0300

    Added translating file to bite code file and executing bite code file.

--- CPU.h
+++ CPU.h
@@ -11,6 +11,65 @@
 #endif //CPU_EMULATOR_STACK_H
 
 namespace cpu {
+    void file_to_bite_code(const std::string& FILE_NAME, const std::string& BITE_CODE_FILE_NAME)
+    {
+        std::map <std::string, int> mapping;
+        mapping["BEGIN"] = 1;
+        mapping["END"]   = 2;
+        mapping["PUSH"]  = 3;
+        mapping["POP"]   = 4;
+        mapping["PUSHR"] = 5;
+        mapping["POPR"]  = 6;
+        mapping["ADD"]   = 7;
+        mapping["SUB"]   = 8;
+        mapping["MUL"]   = 9;
+        mapping["DIV"]   = 10;
+        mapping["OUT"]   = 11;
+        mapping["IN"]    = 12;
+
+        std::ifstream FILE;
+        FILE.open(FILE_NAME);
+        std::ofstream BFILE;
+        BFILE.open(BITE_CODE_FILE_NAME);
+
+        if (!FILE.is_open())
+        {
+            std::cout << "No such file in " << FILE_NAME << std::endl;
+            exit(8);
+        }
+
+        std::string comm;
+        int comm_code;
+
+        while (FILE >> comm)
+        {
+            comm_code = mapping[comm];
+            if (comm_code == 0)
+            {
+                std::cout << "Syntaxis error!" << std::endl;
+                exit(4);
+            }
+            if (comm_code > 0)
+            {
+                BFILE << std::to_string(comm_code);
+                if (comm_code == 3 || comm_code == 5 || comm_code == 6)
+                {
+                    std::string value;
+                    FILE >> value;
+                    for (int i = 0; i < value.length(); i += 1) {
+                        if (!isdigit(value[i])) {
+                            std::cout << "Not a number!";
+                            exit(7);
+                        }
+                    }
+                    BFILE << " " << value << std::endl;
+                }
+                else
+                    BFILE << std::endl;
+            }
+        }
+    }
+
     class cpu {
     public:
         void execute_file(const std::string& FILE_NAME)
@@ -128,51 +187,104 @@ namespace cpu {
             FILE.close();
         }
 
-        void begin() {
-            if (this->exec) {
-                std::cout << "CPU in already running!" << std::endl;
-                exit(1);
+        void execute_bite_code_file(const std::string& BITE_CODE_FILE_NAME)
+        {
+            std::ifstream BFILE;
+            BFILE.open(BITE_CODE_FILE_NAME);
+
+            if (!BFILE.is_open())
+            {
+                std::cout << "No such file in " << BITE_CODE_FILE_NAME << std::endl;
+                exit(8);
             }
+
+            std::string comm;
+            int comm_code;
+
+            while (BFILE >> comm)
+            {
+                comm_code = std::stoi(comm);
+                switch (comm_code)
+                {
+                    case 1: {
+                        this->begin();
+                        break;
+                    }
+                    case 2: {
+                        this->end();
+                        break;
+                    }
+                    case 3: {
+                        std::string value0;
+                        BFILE >> value0;
+                        this->push(std::stoi(value0));
+                        break;
+                    }
+                    case 4: {
+                        this->pop();
+                        break;
+                    }
+                    case 5: {
+                        std::string reg0;
+                        BFILE >> reg0;
+                        this->pushr(std::stoi(reg0));
+                        break;
+                    }
+                    case 6: {
+                        std::string reg0;
+                        BFILE >> reg0;
+                        this->popr(std::stoi(reg0));
+                        break;
+                    }
+                    case 7: {
+                        this->add();
+                        break;
+                    }
+                    case 8: {
+                        this->sub();
+                        break;
+                    }
+                    case 9: {
+                        this->mul();
+                        break;
+                    }
+                    case 10: {
+                        this->div();
+                        break;
+                    }
+                    case 11: {
+                        this->out();
+                        break;
+                    }
+                    case 12: {
+                        this->in();
+                        break;
+                    }
+                }
+            }
+        }
+
+        void begin() {
             this->exec = true;
         }
 
         void end() {
-            if (!this->exec) {
-                std::cout << "CPU in not running!" << std::endl;
-                exit(2);
-            }
             this->exec = false;
         }
 
         void push(int value0) {
-            if (!this->exec) {
-                std::cout << "CPU is not running!" << std::endl;
-                exit(3);
-            }
             this->stack.push(value0);
         }
 
         void pop() {
-            if (!this->exec) {
-                std::cout << "CPU is not running!" << std::endl;
-                exit(3);
-            }
             this->stack.pop();
         }
 
         void pushr(int reg_id) {
-            if (!this->exec) {
-                std::cout << "CPU is not running!" << std::endl;
-                exit(3);
-            }
             this->stack.push(this->reg[reg_id]);
         }
 
         void popr(int reg_id) {
-            if (!this->exec) {
-                std::cout << "CPU is not running!" << std::endl;
-                exit(3);
-            }
             if (this->stack.empty())
             {
                 std::cout << "Stack has to have at least one numbers for POPR!" << std::endl;
@@ -183,10 +295,6 @@ namespace cpu {
         }
 
         void add() {
-            if (!this->exec) {
-                std::cout << "CPU is not running!" << std::endl;
-                exit(3);
-            }
             if (this->stack.empty())
             {
                 std::cout << "Stack has to have at least two numbers for ADD!" << std::endl;
@@ -205,10 +313,6 @@ namespace cpu {
         }
 
         void sub() {
-            if (!this->exec) {
-                std::cout << "CPU is not running!" << std::endl;
-                exit(3);
-            }
             if (this->stack.empty())
             {
                 std::cout << "Stack has to have at least two numbers for SUB!" << std::endl;
@@ -227,10 +331,6 @@ namespace cpu {
         }
 
         void mul() {
-            if (!this->exec) {
-                std::cout << "CPU is not running!" << std::endl;
-                exit(3);
-            }
             if (this->stack.empty())
             {
                 std::cout << "Stack has to have at least two numbers for MUL!" << std::endl;
@@ -249,10 +349,6 @@ namespace cpu {
         }
 
         void div() {
-            if (!this->exec) {
-                std::cout << "CPU is not running!" << std::endl;
-                exit(3);
-            }
             if (this->stack.empty())
             {
                 std::cout << "Stack has to have at least two numbers for DIV!" << std::endl;
@@ -277,10 +373,6 @@ namespace cpu {
 
         void out()
         {
-            if (!this->exec) {
-                std::cout << "CPU is not running!" << std::endl;
-                exit(3);
-            }
             if (this->stack.empty())
             {
                 std::cout << "Stack has to have at least one numbers for OUT!" << std::endl;
@@ -292,10 +384,6 @@ namespace cpu {
 
         void in()
         {
-            if (!this->exec) {
-                std::cout << "CPU is not running!" << std::endl;
-                exit(3);
-            }
             int a;
             std::cin >> a;
             this->stack.push(a);
